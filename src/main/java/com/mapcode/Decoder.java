5923e72f5d5140efa3bcc7ce0421f7a1a43042bc
Mapcode C Developer <pieter.geelen@mapcode.com>
2015-08-30 00:11:18 +0200

    2.1.1 Enforce and test that encode(decode(m)) delivers mapcode m back

--- src/main/java/com/mapcode/Decoder.java
+++ src/main/java/com/mapcode/Decoder.java
@@ -237,8 +237,8 @@ class Decoder {
                                     if (maxx >  decodeLimits.getMinX()) { pt.setMaxLonToMicroDeg(maxx); }
                         
                                     // better?
-                                    if ( pt.getLatDeg() > decodeLimits.getMinY() && pt.getLatMicroDeg() < decodeLimits.getMaxY() &&
-                                         pt.getLonDeg() > decodeLimits.getMinX() && pt.getLonMicroDeg() < decodeLimits.getMaxX() &&
+                                    if ( pt.getLatDeg()*1000000 > decodeLimits.getMinY() && pt.getLatMicroDeg() < decodeLimits.getMaxY() &&
+                                         pt.getLonDeg()*1000000 > decodeLimits.getMinX() && pt.getLonMicroDeg() < decodeLimits.getMaxX() &&
                                          Data.getBoundaries(j).containsPoint(pt)) 
                                     {
                                         result = Point.fromPoint(pt);
